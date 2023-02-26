bc506541b355c09b1be3b6dc1ed941858a6d8f84
Adaiki <grandeur.blog.winner@gmail.com>
2023-02-26 09:15:51 +0900

    Initial Commit

---
+++ myTimer/myTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  myTimerApp.swift
+//  myTimer
+//
+//  Created by 赤荻大輝 on 2023/02/14.
+//
+
+import SwiftUI
+
+@main
+struct myTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
